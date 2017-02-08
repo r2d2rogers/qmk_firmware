935347959e2a8204cdaf12ff578c551feb89e907
Rob Rogers <r2d2rogers@gmail.com>
2017-02-08 15:35:49 -0600

    change mod tap to use MT in keymap

--- keyboards/ergodox/keymaps/r2d2rogers/keymap.c
+++ keyboards/ergodox/keymaps/r2d2rogers/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_EQL,
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
-        KC_FN4, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
+        MT(MOD_LCTL, KC_ESC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_FN0,
         KC_LGUI,KC_LALT,KC_INS, KC_LBRC,KC_RBRC,
                                                 KC_LCTL,KC_LALT,
@@ -321,5 +321,4 @@ const uint16_t fn_actions[] = {
     ACTION_LAYER_SET(2, ON_PRESS),                  // FN1 - set Layer2
     ACTION_LAYER_TOGGLE(3),                         // FN2 - toggle Layer3 aka Numpad layer
     ACTION_LAYER_SET(0, ON_PRESS),                  // FN3 - set Layer0
-    ACTION_MODS_TAP_KEY(MOD_LCTL,KC_ESC),           // FN4 - LCTL with tap ESC
 };
