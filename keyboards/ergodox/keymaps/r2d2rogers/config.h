94c968f09791b7f417df24c1fe56d27e317d66bb
Rob Rogers <r2d2rogers@gmail.com>
2017-02-09 10:18:42 -0600

    remove extra config file options, simplify

--- keyboards/ergodox/keymaps/r2d2rogers/config.h
+++ keyboards/ergodox/keymaps/r2d2rogers/config.h
@@ -4,22 +4,7 @@
 
 #include "../../config.h"
 
-#undef MOUSEKEY_TIME_TO_MAX
-#undef MOUSEKEY_MAX_SPEED
-
-#define MOUSEKEY_TIME_TO_MAX 1
-#define MOUSEKEY_MAX_SPEED 2
-
-#undef MOUSEKEY_DELAY
-#define MOUSEKEY_DELAY 0
-
-#undef MOUSEKEY_WHEEL_DELAY
-#define MOUSEKEY_WHEEL_DELAY 0
-
 #define ONESHOT_TAP_TOGGLE 2
 #define ONESHOT_TIMEOUT 3000
-
-#undef LEADER_TIMEOUT
-#define LEADER_TIMEOUT 1000
 
 #endif
