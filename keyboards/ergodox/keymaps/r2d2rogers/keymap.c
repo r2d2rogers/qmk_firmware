bf3a27a954e74e31600bac27ac04d60e160df314
Rob Rogers <r2d2rogers@gmail.com>
2017-04-28 11:20:43 -0500

    Add visualizer to current setup to test LED

--- keyboards/ergodox/keymaps/r2d2rogers/keymap.c
+++ keyboards/ergodox/keymaps/r2d2rogers/keymap.c
@@ -12,6 +12,13 @@
 #define UTIL 5 // keyboard utility layer
 #define TEMP 6 // Template for new layers
 
+enum custom_keycodes {
+  PLACEHOLDER = SAFE_RANGE, // can always be here
+  EPRM,
+  VRSN,
+  RGB_SLD
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Keymap 0: Basic layer
@@ -320,4 +327,86 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM fn_actions[] = {
   ACTION_SWAP_HANDS_TAP_KEY(KC_A),
   ACTION_SWAP_HANDS_TAP_KEY(KC_G)
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  // MACRODOWN only works in this function
+      switch(id) {
+        case 0:
+        if (record->event.pressed) {
+          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        }
+        break;
+        case 1:
+        if (record->event.pressed) { // For resetting EEPROM
+          eeconfig_init();
+        }
+        break;
+      }
+    return MACRO_NONE;
+};
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+        //printf("action_function() called\n");
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    // dynamically generate these.
+    case EPRM:
+      if (record->event.pressed) {
+        eeconfig_init();
+      }
+      return false;
+      break;
+    case VRSN:
+      if (record->event.pressed) {
+        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+      }
+      return false;
+      break;
+    case RGB_SLD:
+      if (record->event.pressed) {
+        #ifdef RGBLIGHT_ENABLE
+          rgblight_mode(1);
+        #endif
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
+// Runs just one time when the keyboard initializes.
+void matrix_init_user(void) {
+    //start_keyframe_animation(&led_test_animation);
+    //start_keyframe_animation(&led_short_test_animation);
+};
+
+
+// Runs constantly in the background, in a loop.
+void matrix_scan_user(void) {
+
+    uint8_t layer = biton32(layer_state);
+
+    switch (layer) {
+      // TODO: Make this relevant to the ErgoDox EZ.
+        case 2:
+            ergodox_right_led_1_on();
+            break;
+        case 3:
+            ergodox_right_led_2_on();
+            break;
+        case 4:
+            ergodox_right_led_3_on();
+            break;
+        default:
+            ergodox_board_led_off();
+            ergodox_right_led_1_off();
+            ergodox_right_led_2_off();
+            ergodox_right_led_3_off();
+            break;
+    }
+
 };
