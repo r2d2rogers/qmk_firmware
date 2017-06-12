f763c6b2f3f2a399876368e4a92f459c387982df
Rob Rogers <r2d2rogers@gmail.com>
2017-06-12 00:07:38 -0500

    shorten layer names to enhance layout readability

--- keyboards/lets_split/keymaps/r2d2rogers/keymap.c
+++ keyboards/lets_split/keymaps/r2d2rogers/keymap.c
@@ -8,13 +8,13 @@
 
 extern keymap_config_t keymap_config;
 
-#define QWERTY_LAYER 0
-#define LOWER_LAYER 1
-#define UPPER_LAYER 2
-#define SPACEFN_LAYER 3
-#define TENKEY_LAYER 4
-#define NORMAN_LAYER 5
-#define UNDERGLOW_LAYER 6
+#define QWERTY 0
+#define LOWER 1
+#define UPPER 2
+#define SPACEFN 3
+#define TENKEY 4
+#define NORMAN 5
+#define UNDERGLOW 6
 
 // Tap Dance
 enum {
@@ -30,67 +30,67 @@ enum macro_id {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[QWERTY_LAYER] = KEYMAP( \
-  KC_TAB,            KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC, \
-  CTL_T(KC_ESC),     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-  TD(SFT_CAPS),      KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  MT(MOD_RSFT, KC_ENT), \
-  MO(UNDERGLOW_LAYER),  KC_MEH,   KC_LALT,  KC_LGUI,  F(3),     F(1),     F(2),     F(3),  MT(MOD_RGUI, KC_LEFT),  KC_DOWN,  KC_UP,    KC_RGHT \
+[QWERTY] = KEYMAP( \
+KC_TAB       , KC_Q  , KC_W   , KC_E   , KC_R, KC_T, KC_Y, KC_U, KC_I                 , KC_O   , KC_P   , KC_BSPC    , \
+CTL_T(KC_ESC), KC_A  , KC_S   , KC_D   , KC_F, KC_G, KC_H, KC_J, KC_K                 , KC_L   , KC_SCLN, KC_QUOT    , \
+TD(SFT_CAPS) , KC_Z  , KC_X   , KC_C   , KC_V, KC_B, KC_N, KC_M, KC_COMM              , KC_DOT , KC_SLSH, MT(MOD_RSFT, KC_ENT), \
+MO(UNDERGLOW), KC_MEH, KC_LALT, KC_LGUI, F(3), F(1), F(2), F(3), MT(MOD_RGUI, KC_LEFT), KC_DOWN, KC_UP  , KC_RGHT \
 ),
 
-[UPPER_LAYER] = KEYMAP( \
-  KC_GRV,                KC_1,       KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,           KC_0,     _______, \
-  _______,               KC_4,       KC_5,     KC_6,     KC_DOT,   _______,  _______,  KC_MINS,  KC_EQL,   KC_LBRC,        KC_RBRC,  KC_BSLS, \
-  _______,               KC_7,       KC_8,     KC_9,     KC_0,     _______,  _______,  _______,  _______,  KC_DOT,        _______,  _______, \
-  TG(NORMAN_LAYER),     KC_HYPR,    _______,  _______,  _______,  _______,  _______,  _______,  TD(MPLY_MUTE),  KC_VOLD,  KC_VOLU,  KC_MFFD \
+[UPPER] = KEYMAP( \
+  KC_GRV   , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8         , KC_9   , KC_0   , _______  , \
+  _______  , KC_4   , KC_5   , KC_6   , KC_DOT , _______, _______, KC_MINS, KC_EQL       , KC_LBRC, KC_RBRC, KC_BSLS  , \
+  _______  , KC_7   , KC_8   , KC_9   , KC_0   , _______, _______, _______, _______      , KC_DOT , _______, _______  , \
+  TG(NORMAN, KC_HYPR, _______, _______, _______, _______, _______, _______, TD(MPLY_MUTE), KC_VOLD, KC_VOLU, KC_MFFD \
 ),
 
-[LOWER_LAYER] = KEYMAP( \
-  KC_TILD,          KC_EXLM,   KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  _______, \
-  _______,          KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE, \
-  _______,          KC_F7,     KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  _______,  _______,  _______,  _______, \
-  TG(TENKEY_LAYER), KC_HYPR,   _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END \
+[LOWER] = KEYMAP( \
+  KC_TILD   , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______ , \
+  _______   , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE , \
+  _______   , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, _______, _______, _______, _______ , \
+  TG(TENKEY), KC_HYPR, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END \
 ),
 
-[SPACEFN_LAYER] = KEYMAP( \
-  _______, _______, M(M_USERNAME),    _______, LGUI(LSFT(KC_4)), LGUI(LCTL(LSFT(KC_4))),  _______, KC_HOME,           KC_UP,   KC_END,   _______, KC_DELETE, \
-  _______, _______, M(M_RANDDIGIT),   _______, _______,           _______,                KC_PGUP, KC_LEFT,           KC_DOWN, KC_RIGHT, _______, _______, \
-  _______, _______, M(M_RANDLETTER),  _______, _______,           _______,                KC_PGDN, LGUI(LSFT(KC_M)),  _______, _______,  _______, RESET, \
-  _______, _______, _______,          _______, _______,           _______,                _______, _______,           _______, _______,  _______, _______ \
+[SPACEFN] = KEYMAP( \
+  _______, _______, _______, _______, M(M_USERNAME)  , _______, _______, _______, _______, _______, _______, KC_DELETE, \
+  _______, _______, _______, _______, M(M_RANDDIGIT) , _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______  , \
+  _______, _______, _______, _______, M(M_RANDLETTER), _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______, RESET, \
+  _______, _______, _______, _______, _______        , _______, _______, _______, _______, _______, _______, _______ \
 ),
 
-[TENKEY_LAYER] = KEYMAP( \
+[TENKEY] = KEYMAP( \
   _______, _______, _______, _______, _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_PMNS, KC_BSPC, \
   _______, _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, KC_NLCK, \
   _______, _______, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_PDOT, KC_ENT, \
   _______, _______, _______, _______, _______, _______, _______, KC_KP_0, _______, _______, _______, _______ \
 ),
 
-[NORMAN_LAYER] = KEYMAP( \
+[NORMAN] = KEYMAP( \
   _______, KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,    KC_J,    KC_U,    KC_R,    KC_L,    KC_SCLN,  _______, \
   _______, KC_A,    KC_S,    KC_E,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_I,    KC_O,    KC_H,     KC_QUOT, \
   _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P,    KC_M,    KC_COMM, KC_DOT,  _______,  KC_ENT, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
-[UNDERGLOW_LAYER] = KEYMAP( \
-  _______,  RGB_HUD,  RGB_HUI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
-  AU_OFF,   RGB_SAD,  RGB_SAI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
-  AU_ON,    RGB_VAD,  RGB_VAI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, \
-  _______,  RGB_TOG,  RGB_MOD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______ \
+[UNDERGLOW] = KEYMAP( \
+  _______, RGB_HUD, RGB_HUI, _______, _______, _______, _______, _______, _______, _______, _______, _______  , \
+  AU_OFF , RGB_SAD, RGB_SAI, _______, _______, _______, _______, _______, _______, _______, _______, _______  , \
+  AU_ON  , RGB_VAD, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______  , \
+  _______, RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_DEFAULT_LAYER_SET(QWERTY_LAYER),
+    [0] = ACTION_DEFAULT_LAYER_SET(QWERTY),
 
     // Tap for backspace, hold for LOWER
-    [1] = ACTION_LAYER_TAP_KEY(LOWER_LAYER, KC_BSPC),
+    [1] = ACTION_LAYER_TAP_KEY(LOWER, KC_BSPC),
 
     // Tap for enter, hold for RAISE
-    [2] = ACTION_LAYER_TAP_KEY(UPPER_LAYER, KC_ENT),
+    [2] = ACTION_LAYER_TAP_KEY(UPPER, KC_ENT),
 
     // Tap for space, hold for SpaceFN
-    [3] = ACTION_LAYER_TAP_KEY(SPACEFN_LAYER, KC_SPC),
+    [3] = ACTION_LAYER_TAP_KEY(SPACEFN, KC_SPC),
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
