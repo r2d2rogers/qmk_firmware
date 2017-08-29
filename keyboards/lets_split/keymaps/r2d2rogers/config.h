f38af588b84b7e17d48b7682ae9c418e9f808675
Rob Rogers <r2d2rogers@gmail.com>
2017-08-28 22:51:55 -0500

    rearrange config.h

--- keyboards/lets_split/keymaps/r2d2rogers/config.h
+++ keyboards/lets_split/keymaps/r2d2rogers/config.h
@@ -1,33 +1,28 @@
 #ifndef CONFIG_USER_H
-#define CONFIG_USER_H
+        #define CONFIG_USER_H
 
-#include "../../config.h"
+        #include "../../config.h"
 
-#undef RGBLED_NUM
-#define RGBLED_NUM 32
+        #define TAPPING_TERM 100
 
-#define BACKLIGHT_LEVELS 10
+        #define USE_SERIAL
+        #define EE_HANDS
 
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
+        #undef RGBLED_NUM
+        #define RGBLED_NUM 32
+        #define RGBLIGHT_ANIMATIONS
+        #define RGBLIGHT_EFFECT_KNIGHT_OFFSET 32
+        #define RGBLIGHT_EFFECT_DUALKNIGHT_LENGTH 6
 
-#define RGBLIGHT_ANIMATIONS
-#define RGBLIGHT_EFFECT_KNIGHT_OFFSET 32
-#define RGBLIGHT_EFFECT_DUALKNIGHT_LENGTH 6
+        #define _______ KC_TRNS
+        #define XXXXXXX KC_NO
 
-#define USE_SERIAL
-#define EE_HANDS
+        #define randadd 53
+        #define randmul 181
+        #define randmod 167
 
-//#define C6_AUDIO
-
-#define randadd 53
-#define randmul 181
-#define randmod 167
-
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
-
-#define TAPPING_TERM 200
+        #ifdef AUDIO_ENABLE
+                #define C6_AUDIO
+        #endif
 
 #endif
