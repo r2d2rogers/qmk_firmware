5401dc86552880b86979dc30d2b59c41f847244f
Rob Rogers <r2d2rogers@gmail.com>
2017-08-30 21:31:47 -0500

    mv makefile to rules.mk

---
+++ keyboards/lets_split/keymaps/r2d2rogers/rules.mk
@@ -0,0 +1,9 @@
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+AUDIO_ENABLE = no           # Audio output on port C6
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+TAP_DANCE_ENABLE = yes
+USE_I2C = no
+
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
