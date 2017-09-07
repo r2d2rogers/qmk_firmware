b690b7d96e4c6cb421b537c2873e2e822ba096b2
Rob Rogers <r2d2rogers@gmail.com>
2017-09-07 14:00:34 -0500

    move hand swap from ; to ', both will be on the outer columns of the keyboard

--- keyboards/lets_split/keymaps/r2d2rogers/keymap.c
+++ keyboards/lets_split/keymaps/r2d2rogers/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [QWERTY] = KEYMAP( \
     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,      KC_Y   , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSPC, \
-CTL_T(KC_ESC), KC_A, KC_S  , KC_D  , KC_F  , KC_G  ,      F(5)   , KC_J  , KC_K  , KC_L  , F(4)  ,KC_QUOT, \
+CTL_T(KC_ESC), KC_A, KC_S  , KC_D  , KC_F  , KC_G  ,      F(5)   , KC_J  , KC_K  , KC_L  ,KC_SCLN, F(4)  , \
 TD(SFT_CAPS) , KC_Z, KC_X  , KC_C  , KC_V  , KC_B  ,      KC_N   , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,MT(MOD_RSFT, KC_ENT), \
  MO(ADJUST), KC_MEH,KC_LALT,KC_LGUI, F(3)  , F(1)  ,      F(2)   , F(3)  ,
                                                             MT(MOD_RGUI, KC_LEFT),KC_DOWN, KC_UP ,KC_RGHT \
@@ -157,7 +157,7 @@ const uint16_t PROGMEM fn_actions[] = {
 
     // Tap for space, hold for SpaceFN
     [3] = ACTION_LAYER_TAP_KEY(SPACEFN, KC_SPC),
-    [4] = ACTION_SWAP_HANDS_TAP_KEY(KC_SCLN),
+    [4] = ACTION_SWAP_HANDS_TAP_KEY(KC_QUOT),
     [5] = ACTION_SWAP_HANDS_TAP_KEY(KC_H)
 };
 
