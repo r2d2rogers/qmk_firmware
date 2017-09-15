4d94c5c7b125b582f21297add90e399747b00327
Rob Rogers <r2d2rogers@gmail.com>
2017-09-15 16:22:52 -0500

    it's rules.mk now! and RGB keys added on right

--- keyboards/lets_split/keymaps/r2d2rogers/config.h
+++ keyboards/lets_split/keymaps/r2d2rogers/config.h
@@ -1,33 +1,43 @@
 #ifndef CONFIG_USER_H
-        #define CONFIG_USER_H
+#define CONFIG_USER_H
 
-        #include "../../config.h"
+#include "../../config.h"
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
 
-        #undef TAPPING_TERM
-        #define TAPPING_TERM 200
+/* Use I2C or Serial, not both */
 
-        #define ONESHOT_TAP_TOGGLE 2
-        #define ONESHOT_TIMEOUT 3000
+#define USE_SERIAL
+// #define USE_I2C
 
-        #define USE_SERIAL
-        #define EE_HANDS
+/* Select hand configuration */
 
-        #ifdef RGBLIGHT_ENABLE
-                #undef RGBLED_NUM
-                #define RGBLED_NUM 32
-                #define RGBLIGHT_ANIMATIONS
-                #define RGBLIGHT_EFFECT_KNIGHT_OFFSET 32
-        #endif
+// #define MASTER_LEFT
+// #define _MASTER_RIGHT
+#define EE_HANDS
 
-        #define _______ KC_TRNS
-        #define XXXXXXX KC_NO
 
-        #define randadd 53
-        #define randmul 181
-        #define randmod 167
+#ifdef RGBLIGHT_ENABLE
+#undef RGBLED_NUM
+#define RGBLED_NUM 32
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_SNAKE_LENGTH 4
+#define RGBLIGHT_EFFECT_KNIGHT_LENGTH 3
+#define RGBLIGHT_EFFECT_KNIGHT_OFFSET 8
+#define RGBLIGHT_EFFECT_KNIGHT_LED_NUM 16
+#define RGBLIGHT_EFFECT_CHRISTMAS_INTERVAL 500
+#define RGBLIGHT_EFFECT_CHRISTMAS_STEP 3
+#endif
 
-        #ifdef AUDIO_ENABLE
-                #define C6_AUDIO
-        #endif
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+#define randadd 53
+#define randmul 181
+#define randmod 167
+
+#ifdef AUDIO_ENABLE
+#define C6_AUDIO
+#endif
 
 #endif
