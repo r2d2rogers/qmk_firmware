58610f45cd3f49569b9039316dbbcd53e6b270c8
Rob Rogers <r2d2rogers@gmail.com>
2017-11-14 22:14:03 -0600

    user function notes

--- users/r2d2rogers/r2d2rogers.c
+++ users/r2d2rogers/r2d2rogers.c
@@ -1,1 +1,13 @@
 #include "r2d2rogers.h"
+
+/* IDEAS
+ *
+ * Macros, Tapdance,
+ * Username short cut
+ * make command, shift for make and flash
+ * Layers, names purpose?
+ * sounds
+ * lights
+ * joystick
+ *
+ */
