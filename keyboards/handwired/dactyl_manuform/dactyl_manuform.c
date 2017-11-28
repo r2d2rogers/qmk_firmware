e4649d69f53fc1cdd4983fa2ef7552ec72248d53
Rob Rogers <r2d2rogers@gmail.com>
2017-11-28 16:43:43 -0600

    Get pointing device workin as mouse

--- keyboards/handwired/dactyl_manuform/dactyl_manuform.c
+++ keyboards/handwired/dactyl_manuform/dactyl_manuform.c
@@ -1,4 +1,8 @@
 #include "dactyl_manuform.h"
+#include "pointing_device.h"
+#include "report.h"
+#include "analog.h"
+
 
 int8_t readaxis(uint16_t axis){
     int8_t reaxis = ((axis - 16) >> 5) - 15;
@@ -27,19 +31,19 @@ void pointing_device_task(void){
 //shifting and transferring the info to the mouse report varaible
 
 //mouseReport.x = 127 max -127 min
-        //currentReport.x = readaxis(analogRead(3));
-        currentReport.x = 0;
+        currentReport.x = readaxis(analogRead(3)) * -1;
+        //currentReport.x = 0;
 
 //mouseReport.y = 127 max -127 min
-        //currentReport.y = readaxis(analogRead(2)) * -1;
-        currentReport.y = 0;
+        currentReport.y = readaxis(analogRead(2)) * -1;
+        //currentReport.y = 0;
 
 //mouseReport.v = 127 max -127 min (scroll vertical)
-        //currentReport.v = 0;
-        currentReport.v = readaxis(analogRead(2));
+        currentReport.v = 0;
+        //currentReport.v = readaxis(analogRead(2));
 //mouseReport.h = 127 max -127 min (scroll horizontal)
-        //currentReport.h = 0;
-        currentReport.h = readaxis(analogRead(3));
+        currentReport.h = 0;
+        //currentReport.h = readaxis(analogRead(3));
 
         currentReport.buttons = 0x00;
 
