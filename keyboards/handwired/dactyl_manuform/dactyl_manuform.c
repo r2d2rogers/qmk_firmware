0434247e007ec5c15a58ed7480b28ecabe88d9ff
Rob Rogers <r2d2rogers@gmail.com>
2017-12-15 19:15:39 -0600

    WIP vector joystick

--- keyboards/handwired/dactyl_manuform/dactyl_manuform.c
+++ keyboards/handwired/dactyl_manuform/dactyl_manuform.c
@@ -2,6 +2,7 @@
 #include "pointing_device.h"
 #include "report.h"
 #include "analog.h"
+#include "math.h"
 
 
 int8_t readaxis(uint16_t axis){
@@ -23,6 +24,19 @@ int8_t readaxis(uint16_t axis){
     return 0;
 }
 
+
+/*
+	{
+float deadzone = 0.25f;
+    Vector2 stickInput = new Vector2(Input.GetAxis(“Horizontal”), Input.GetAxis(“Vertical”));
+    if(stickInput.magnitude < deadzone)
+        stickInput = Vector2.zero;
+    else
+        stickInput = stickInput.normalized * ((stickInput.magnitude - deadzone) / (1 - deadzone));
+
+}
+*/
+
 void pointing_device_task(void){
         report_mouse_t currentReport = {};
 
@@ -30,20 +44,21 @@ void pointing_device_task(void){
         currentReport = pointing_device_get_report();
 //shifting and transferring the info to the mouse report varaible
 
-//mouseReport.x = 127 max -127 min
+        //mouseReport.x = 127 max -127 min
         currentReport.x = readaxis(analogRead(2));
         //currentReport.x = 0;
 
-//mouseReport.y = 127 max -127 min
+        //mouseReport.y = 127 max -127 min
         currentReport.y = readaxis(analogRead(3)) * -1;
         //currentReport.y = 0;
 
-//mouseReport.v = 127 max -127 min (scroll vertical)
+        //mouseReport.v = 127 max -127 min (scroll vertical)
         currentReport.v = 0;
         //currentReport.v = readaxis(analogRead(2));
-//mouseReport.h = 127 max -127 min (scroll horizontal)
-        currentReport.h = 0;
+
+        //mouseReport.h = 127 max -127 min (scroll horizontal)
         //currentReport.h = readaxis(analogRead(3));
+        currentReport.h = 0;
 
         currentReport.buttons = 0x00;
 
