303407482d0b97d35cd40a6571db752fff901207
Rob Rogers <r2d2rogers@gmail.com>
2018-07-04 12:03:12 -0500

    I2C message modification to pass Analog stick values

--- keyboards/lets_split/matrix.c
+++ keyboards/lets_split/matrix.c
@@ -216,10 +216,16 @@ int i2c_transaction(void) {
 
     if (!err) {
         int i;
-        for (i = 0; i < ROWS_PER_HAND-1; ++i) {
+        for (i = 0; i < ROWS_PER_HAND; ++i) {
             matrix[slaveOffset+i] = i2c_master_read(I2C_ACK);
         }
-        matrix[slaveOffset+i] = i2c_master_read(I2C_NACK);
+        uint8_t i2c1, i2c2, i2c3;
+        i2c1 = i2c_master_read(I2C_ACK);
+        i2c2 = i2c_master_read(I2C_ACK);
+        i2c3 = i2c_master_read(I2C_NACK);
+        uint16_t analogX = (i2c1 << 2) & (i2c3 >> 6);
+        uint16_t analogY = (i2c2 << 2) & (0x0C & (i2c3 >> 4));
+        bool buttonPressed = (i2c3 & 0x01);
         i2c_master_stop();
     } else {
 i2c_error: // the cable is disconnceted, or something else went wrong
@@ -285,6 +291,11 @@ void matrix_slave_scan(void) {
     for (int i = 0; i < ROWS_PER_HAND; ++i) {
         i2c_slave_buffer[i] = matrix[offset+i];
     }
+    i2c_slave_buffer[ROWS_PER_HAND + 1] = (uint8_t)(analogValueX >> 2);
+    i2c_slave_buffer[ROWS_PER_HAND + 2] = (uint8_t)(analogValueY >> 2);
+    i2c_slave_buffer[ROWS_PER_HAND + 3] = (uint8_t)((analogValueX & 0x0003) << 6);
+    i2c_slave_buffer[ROWS_PER_HAND + 3] |= (uint8_t)((analogValueY & 0x0003) << 4);
+    i2c_slave_buffer[ROWS_PER_HAND + 3] |= joystickDepressed?0x01:0x00;
 #else // USE_SERIAL
     for (int i = 0; i < ROWS_PER_HAND; ++i) {
         serial_slave_buffer[i] = matrix[offset+i];
