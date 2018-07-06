492ca9d1a997ae600776649633f5a873457fc785
Rob Rogers <r2d2rogers@gmail.com>
2018-07-06 14:43:13 -0500

    Broken build WIP ANALOG_STICK Let's split pt.5 My static vars

--- quantum/analog_stick.c
+++ quantum/analog_stick.c
@@ -17,18 +17,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "analog_stick.h"
 
-int8_t readaxis(uint16_t analog_pin){
-  int8_t axis_value = ((analog_pin - 16) >> 5) - 15;
+int8_t readaxis(uint16_t analogValue){
+  int8_t axisValue = ((analogValue - 16) >> 5) - 15;
   uint8_t deadzone = ANALOG_DEADZONE;
-  if(axis_value < 0){
-    if(axis_value < - deadzone ){
-      return axis_value + deadzone;
+  if(axisValue < 0){
+    if(axisValue < - deadzone ){
+      return axisValue + deadzone;
     }else{
       return 0;
     }
-  }else if(axis_value > 0){
-    if(axis_value > deadzone ){
-      return axis_value - deadzone;
+  }else if(axisValue > 0){
+    if(axisValue > deadzone ){
+      return axisValue - deadzone;
     }else{
       return 0;
     }
