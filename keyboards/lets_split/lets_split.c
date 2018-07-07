b915d35c32767ad75f2ba36105d1463891ed16b1
Rob Rogers <r2d2rogers@gmail.com>
2018-07-07 13:24:44 -0500

    move the pointing device task back to keymap and add some more deadzone

--- keyboards/lets_split/lets_split.c
+++ keyboards/lets_split/lets_split.c
@@ -1,47 +1,5 @@
 #include "lets_split.h"
 
-#ifdef POINTING_DEVICE_ENABLE
-#ifdef ANALOG_STICK_ENABLE
-#include "pointing_device.h"
-#include "report.h"
-#include "analog.h"
-
-extern uint16_t analogX;
-extern uint16_t analogY;
-extern bool buttonPressed;
-
-#include "analog_stick.h"
-#include "analog.h"
-
-void pointing_device_task(void){
-  report_mouse_t currentReport = {};
-
-  currentReport = pointing_device_get_report();
-  //shifting and transferring the info to the mouse report varaible
-
-  //mouseReport.x = 127 max -127 min
-  //currentReport.x = readaxis(analogRead(ANALOG_X_PIN));
-  currentReport.x = parseaxis(readaxis(ANALOG_X_PIN));
-
-  //mouseReport.y = 127 max -127 min
-  //currentReport.y = readaxis(analogRead(ANALOG_Y_PIN)) * -1;
-  currentReport.y = parseaxis(readaxis(ANALOG_Y_PIN));
-
-  //mouseReport.v = 127 max -127 min (scroll vertical)
-  currentReport.v = parseaxis(analogY);
-  //mouseReport.h = 127 max -127 min (scroll horizontal)
-  currentReport.h = parseaxis(analogX);
-
-  //currentReport.buttons = 0x00;
-  currentReport.buttons = buttonPressed;
-
-  pointing_device_set_report(currentReport);
-
-  pointing_device_send();
-}
-#endif
-#endif
-
 #ifdef SWAP_HANDS_ENABLE
 __attribute__ ((weak))
 const keypos_t hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
