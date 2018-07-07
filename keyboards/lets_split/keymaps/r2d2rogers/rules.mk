9b731448699527ad738d3bea5e41e5a8e424bd26
Rob Rogers <r2d2rogers@gmail.com>
2018-07-07 00:34:05 -0500

    cleanup to make prettier diff

--- keyboards/lets_split/keymaps/r2d2rogers/rules.mk
+++ keyboards/lets_split/keymaps/r2d2rogers/rules.mk
@@ -3,21 +3,21 @@
 #   the appropriate keymap folder that will get included automatically
 
 
+MOUSEKEY_ENABLE   = no  # Mouse keys(+4700)
+EXTRAKEY_ENABLE   = yes # Audio control and System control(+450)
+CONSOLE_ENABLE    = no         # Console for debug(+400)
+COMMAND_ENABLE    = no        # Commands for debug and configuration
+TAP_DANCE_ENABLE  = no
+RGBLIGHT_ENABLE   = yes  # Enable WS2812 RGB underlight.
+AUDIO_ENABLE      = no # Audio output on port C6
+NKRO_ENABLE       = yes # Nkey Rollover - see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 
-EXTRAKEY_ENABLE        = yes # Audio control and System control(+450)
-CONSOLE_ENABLE         = no         # Console for debug(+400)
-COMMAND_ENABLE         = no        # Commands for debug and configuration
-TAP_DANCE_ENABLE       = no
-RGBLIGHT_ENABLE        = yes  # Enable WS2812 RGB underlight.
-AUDIO_ENABLE           = no # Audio output on port C6
-NKRO_ENABLE            = yes # Nkey Rollover - see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+ONEHAND_ENABLE    = yes # Allow swapping hands of keyboard
+INDICATOR_LIGHTS  = no
 
-ONEHAND_ENABLE         = yes # Allow swapping hands of keyboard
-INDICATOR_LIGHTS       = no
-
-RGBLIGHT_TWINKLE       = yes
-RGB_MATRIX_ENABLE      = no
+RGBLIGHT_TWINKLE  = yes
+RGB_MATRIX_ENABLE = no
 POINTING_DEVICE_ENABLE = yes
-USE_I2C                = yes
-ANALOG_STICK_ENABLE    = yes
+USE_I2C           = yes
+ANALOG_STICK_ENABLE = yes
 
