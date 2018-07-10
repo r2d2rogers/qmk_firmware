852ec839370d6ad33e0a094e4087bc354421e853
Rob Rogers <robr@nuby.com>
2018-07-10 13:38:15 -0500

    continued debugging

--- keyboards/lets_split/keymaps/r2d2rogers/keymap.c
+++ keyboards/lets_split/keymaps/r2d2rogers/keymap.c
@@ -208,7 +208,7 @@ void matrix_init_keymap(void){
     debug_keyboard=true;
 };
 
-void slave_scan_user(void){
+void slave_scan_keymap(void){
   analogX = 512;
   analogY = 512;
   //analogX = readaxis(ANALOG_X_PIN);
@@ -225,16 +225,21 @@ void pointing_device_task(void){
 
   //mouseReport.x = 127 max -127 min
   //currentReport.x = readaxis(analogRead(ANALOG_X_PIN));
-  currentReport.x = parseaxis(readaxis(ANALOG_X_PIN));
+  //currentReport.x = parseaxis(readaxis(ANALOG_X_PIN));
+  currentReport.x = 0;
 
   //mouseReport.y = 127 max -127 min
   //currentReport.y = readaxis(analogRead(ANALOG_Y_PIN)) * -1;
-  currentReport.y = parseaxis(readaxis(ANALOG_Y_PIN));
+  //currentReport.y = parseaxis(readaxis(ANALOG_Y_PIN)) * -1;
+  currentReport.y = 0;
 
   //mouseReport.v = 127 max -127 min (scroll vertical)
-  currentReport.v = parseaxis(analogY);
+  //currentReport.v = parseaxis(analogY);
+  currentReport.v = 0;
+
   //mouseReport.h = 127 max -127 min (scroll horizontal)
-  currentReport.h = parseaxis(analogX);
+  //currentReport.h = parseaxis(analogX);
+  currentReport.h = 0;
 
   //currentReport.buttons = 0x00;
   currentReport.buttons = buttonPressed;
