272f88c0894d96329cf9d28b141ed1283c3e93be
Rob Rogers <r2d2rogers@gmail.com>
2018-07-15 23:53:16 -0500

    move variable init to analog_stick.h

--- quantum/analog_stick.h
+++ quantum/analog_stick.h
@@ -17,6 +17,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef ANALOG_STICK_H
 #define ANALOG_STICK_H
 
+uint16_t analogX;
+uint16_t analogY;
+bool buttonPressed;
+uint16_t analogValue;
+int16_t axis_value;
+uint8_t deadzone;
+
 void analog_stick_init(void);
 uint16_t readaxis(uint8_t analogPin);
 int8_t parseaxis(uint16_t analogValue);
