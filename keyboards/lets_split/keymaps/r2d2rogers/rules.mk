e833a1583df0a9ef76d748318959a0d3281c6933
Rob Rogers <r2d2rogers@gmail.com>
2018-07-20 13:40:28 -0500

    disable RGBLIGHT while working on rgb matrix for WIP testing

--- keyboards/lets_split/keymaps/r2d2rogers/rules.mk
+++ keyboards/lets_split/keymaps/r2d2rogers/rules.mk
@@ -8,14 +8,14 @@ EXTRAKEY_ENABLE   = yes # Audio control and System control(+450)
 CONSOLE_ENABLE    = no         # Console for debug(+400)
 COMMAND_ENABLE    = no        # Commands for debug and configuration
 TAP_DANCE_ENABLE  = no
-RGBLIGHT_ENABLE   = yes  # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE   = no  # Enable WS2812 RGB underlight.
 AUDIO_ENABLE      = yes # Audio output on port C6
 NKRO_ENABLE       = yes # Nkey Rollover - see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 
 ONEHAND_ENABLE    = yes # Allow swapping hands of keyboard
 INDICATOR_LIGHTS  = no
 
-RGBLIGHT_TWINKLE  = yes
+RGBLIGHT_TWINKLE  = no
 RGB_MATRIX_ENABLE = yes
 POINTING_DEVICE_ENABLE = no
 USE_I2C           = yes
