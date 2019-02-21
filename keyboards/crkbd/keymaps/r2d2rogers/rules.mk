df3fb46ff99938b155861a2697c6a401bb0862b1
Rob Rogers <r2d2rogers@gmail.com>
2019-02-21 11:00:53 -0600

    Add Velocikey to crkbd

--- keyboards/crkbd/keymaps/r2d2rogers/rules.mk
+++ keyboards/crkbd/keymaps/r2d2rogers/rules.mk
@@ -11,13 +11,14 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
-AUDIO_ENABLE = yes           # Audio output on port C6
+AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 RGBLIGHT_TWINKLE  = no
 INDICATOR_LIGHTS  = no
+VELOCIKEY_ENABLE = yes
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
