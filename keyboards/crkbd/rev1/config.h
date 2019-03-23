0bbf8b8ed1ce5f6ff6ce77c199529053c266794d
Rob Rogers <r2d2rogers@gmail.com>
2019-03-23 12:24:14 -0500

    Add other half of the matrix definition for the CRKBD

--- keyboards/crkbd/rev1/config.h
+++ keyboards/crkbd/rev1/config.h
@@ -53,7 +53,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* ws2812 RGB LED */
 #define RGB_DI_PIN D3
 
-#define RGBLED_NUM 27    // Number of LEDs
+#define RGBLED_NUM 54    // Number of LEDs
 #define DRIVER_COUNT 1
 #define DRIVER_LED_TOTAL RGBLED_NUM
 
