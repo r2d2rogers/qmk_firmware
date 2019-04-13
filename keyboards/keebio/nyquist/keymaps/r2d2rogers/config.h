326bfc4e3af3dbbdc3142370d597b962385e67e6
Rob Rogers <r2d2rogers@gmail.com>
2019-04-13 15:48:00 -0500

    WIP cleanup

--- keyboards/keebio/nyquist/keymaps/r2d2rogers/config.h
+++ keyboards/keebio/nyquist/keymaps/r2d2rogers/config.h
@@ -53,7 +53,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define RGB_DI_PIN D3
 #undef RGBLED_NUM
 #define RGBLED_NUM 24
-#define RGBLIGHT_ANIMATIONS
+//#define RGBLIGHT_ANIMATIONS
 #endif
 
 #ifdef RGBLIGHT_ANIMATIONS
