8b3e40984deec29447a1b9925838b251e93b032f
Rob Rogers <r2d2rogers@gmail.com>
2019-05-17 15:11:12 -0500

    modify lighting

--- keyboards/crkbd/keymaps/chilok/keymap.c
+++ keyboards/crkbd/keymaps/chilok/keymap.c
@@ -22,9 +22,9 @@ extern uint8_t is_master;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _LOWER 3
-#define _RAISE 4
-#define _ADJUST 16
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 3
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -62,44 +62,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //┌────────┬────────┬────────┬────────┬────────┬─────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T    , KC_Y    , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-        MO(3)  , KC_A   , KC_S   , KC_D   , KC_F   , KC_G    , KC_H    , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+        MO(1)  , KC_A   , KC_S   , KC_D   , KC_F   , KC_G    , KC_H    , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B    , KC_N    , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS,
     //└────────┴────────┴────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┴────────┴────────┘
-                                   KC_LALT, KC_LCTL,   KC_SPC,   KC_ENT, KC_RGUI, TT(4)
+                                   KC_LALT, KC_LCTL, KC_SPC  ,   KC_ENT, KC_RGUI, TT(2)
     //                           └────────┴────────┴─────────┘└────────┴────────┴────────┘
       ),
   [_LOWER] = LAYOUT( \
     //┌────────┬────────┬────────┬────────┬────────┬─────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
-KC_ESC, KC_VOLD, KC_UP, KC_VOLU, KC_NO, RGB_TOG, KC_NO, KC_BTN1, KC_MS_U, KC_BTN2, KC_PSCR, KC_DEL,
+        KC_ESC , KC_VOLD, KC_UP  , KC_VOLU, KC_NO  , RGB_TOG ,  KC_NO  , KC_BTN1, KC_MS_U, KC_BTN2, KC_PSCR, KC_DEL ,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, RGB_M_SW, KC_PGUP, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO, KC_CAPS,
+        KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO  , RGB_M_SW,  KC_PGUP, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO  , KC_CAPS,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-KC_LSFT, KC_MPRV, KC_MNXT, KC_MPLY, KC_NO, KC_MUTE, KC_PGDN, KC_HOME, KC_END, KC_NO, KC_NO, KC_NLCK,
+        KC_LSFT, KC_MPRV, KC_MNXT, KC_MPLY, KC_NO  , KC_MUTE ,  KC_PGDN, KC_HOME, KC_END , KC_NO  , KC_NO  , KC_NLCK,
     //└────────┴────────┴────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┴────────┴────────┘
-KC_MINS, KC_EQL, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC
+                                   KC_MINS, KC_EQL , KC_LPRN ,  KC_RPRN, KC_LBRC, KC_RBRC
     //                           └────────┴────────┴─────────┘└────────┴────────┴────────┘
       ),
   [_RAISE] = LAYOUT( \
     //┌────────┬────────┬────────┬────────┬────────┬─────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
-KC_ESC, KC_NO, KC_P7, KC_P8, KC_P9, KC_PSLS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_BSPC,
+        KC_ESC,  KC_NO  , KC_P7  , KC_P8  , KC_P9  , KC_PSLS ,  KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_BSPC,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-KC_BSPC, KC_PCMM, KC_P4, KC_P5, KC_P6, KC_PAST, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
+        KC_BSPC, KC_PCMM, KC_P4  , KC_P5  , KC_P6  , KC_PAST ,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-KC_PENT, KC_P0, KC_P1, KC_P2, KC_P3, KC_PMNS, KC_UNDS, KC_PLUS, KC_PIPE, KC_LCBR, KC_RCBR, KC_CALC,
+        KC_PENT, KC_P0  , KC_P1  , KC_P2  , KC_P3  , KC_PMNS ,  KC_UNDS, KC_PLUS, KC_PIPE, KC_LCBR, KC_RCBR, KC_CALC,
     //└────────┴────────┴────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┴────────┴────────┘
-KC_PDOT, KC_PPLS, KC_SPC, KC_ENT, TG(16), KC_TRNS
+                                   KC_PDOT, KC_PPLS, KC_SPC  ,  KC_ENT , TG(3) , KC_TRNS
     //                           └────────┴────────┴─────────┘└────────┴────────┴────────┘
       ),
   [_ADJUST] = LAYOUT( \
     //┌────────┬────────┬────────┬────────┬────────┬─────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
-          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_M_R,
+          KC_NO, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,   RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_MOD,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_M_SW,
+          KC_NO, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,   KC_NO  , RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_RMOD,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO, RGB_RMOD, VLK_TOG,   KC_NO,   KC_NO, RGB_M_P, RGB_M_G,
+          KC_NO, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
     //└────────┴────────┴────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┴────────┴────────┘
-                                     KC_NO,   KC_NO,    KC_NO,    KC_NO,  TG(16), KC_TRNS
+                                   KC_NO  , KC_NO  , KC_NO   ,  RGB_MOD, TG(3) , KC_TRNS
     //                           └────────┴────────┴─────────┘└────────┴────────┴────────┘
   )
 };
--- keyboards/crkbd/keymaps/chilok/rules.mk
+++ keyboards/crkbd/keymaps/chilok/rules.mk
@@ -14,10 +14,11 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = WS2812
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 TAP_DANCE_ENABLE = no
-VELOCIKEY_ENABLE = yes
+VELOCIKEY_ENABLE = no
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
