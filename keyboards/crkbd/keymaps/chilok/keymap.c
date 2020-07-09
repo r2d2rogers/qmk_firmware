1ce5e770b6713e8915edfca0720a0174788ec40b
Rob Rogers <r2d2rogers@gmail.com>
2020-07-09 15:57:53 -0500

    modify keymap

--- keyboards/crkbd/keymaps/chilok/keymap.c
+++ keyboards/crkbd/keymaps/chilok/keymap.c
@@ -48,40 +48,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B    , KC_N    , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS,
     //└────────┴────────┴────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┴────────┴────────┘
-                                   KC_LALT, KC_LCTL, KC_SPC  ,   KC_ENT, KC_RGUI, TT(2)
+                                   KC_LALT, KC_LCTL, LT(2,KC_SPC),KC_ENT,KC_RGUI, MO(_RAISE)
     //                           └────────┴────────┴─────────┘└────────┴────────┴────────┘
       ),
-  [_LOWER] = LAYOUT( \
+  [_LOWER] = LAYOUT(
     //┌────────┬────────┬────────┬────────┬────────┬─────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
-        KC_ESC , KC_VOLD, KC_UP  , KC_VOLU, KC_NO  , RGB_TOG ,  KC_NO  , KC_BTN1, KC_MS_U, KC_BTN2, KC_PSCR, KC_DEL ,
+        KC_MUTE, KC_VOLD, KC_UP  , KC_VOLU, BL_TOGG, RGB_TOG ,  KC_NO  , KC_BTN1, KC_WH_U, KC_BTN2, KC_PSCR, KC_DEL ,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO  , RGB_M_SW,  KC_PGUP, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO  , KC_CAPS,
+        KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, BL_STEP, RGB_MOD ,  KC_PGUP, KC_WH_L, KC_WH_D, KC_WH_R, KC_NO  , KC_NO  ,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_LSFT, KC_MPRV, KC_MNXT, KC_MPLY, KC_NO  , KC_MUTE ,  KC_PGDN, KC_HOME, KC_END , KC_NO  , KC_NO  , KC_NLCK,
+        KC_LSFT, KC_MRWD, KC_MFFD, KC_MPLY, KC_NO  , RGB_RMOD,  KC_PGDN, KC_HOME, KC_END , KC_NO  , KC_NLCK, KC_CAPS,
     //└────────┴────────┴────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┴────────┴────────┘
                                    KC_MINS, KC_EQL , KC_LPRN ,  KC_RPRN, KC_LBRC, KC_RBRC
     //                           └────────┴────────┴─────────┘└────────┴────────┴────────┘
       ),
-  [_RAISE] = LAYOUT( \
+  [_RAISE] = LAYOUT(
     //┌────────┬────────┬────────┬────────┬────────┬─────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
-        KC_ESC,  KC_NO  , KC_P7  , KC_P8  , KC_P9  , KC_PSLS ,  KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_BSPC,
+        KC_ESC , KC_CALC, KC_P7  , KC_P8  , KC_P9  , KC_PSLS ,  KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_BSPC,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
         KC_BSPC, KC_PCMM, KC_P4  , KC_P5  , KC_P6  , KC_PAST ,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-        KC_PENT, KC_P0  , KC_P1  , KC_P2  , KC_P3  , KC_PMNS ,  KC_UNDS, KC_PLUS, KC_PIPE, KC_LCBR, KC_RCBR, KC_CALC,
+        KC_ENT , KC_P0  , KC_P1  , KC_P2  , KC_P3  , KC_PMNS ,  KC_UNDS, KC_PLUS, KC_PIPE, KC_LCBR, KC_RCBR, KC_CALC,
     //└────────┴────────┴────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┴────────┴────────┘
-                                   KC_PDOT, KC_PPLS, KC_SPC  ,  KC_ENT , TG(3) , KC_TRNS
+                                   KC_PDOT, KC_PPLS, KC_TRNS ,  KC_ENT , TG(3)  , KC_TRNS
     //                           └────────┴────────┴─────────┘└────────┴────────┴────────┘
       ),
-  [_ADJUST] = LAYOUT( \
+  [_ADJUST] = LAYOUT(
     //┌────────┬────────┬────────┬────────┬────────┬─────────┐┌────────┬────────┬────────┬────────┬────────┬────────┐
-          KC_NO, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,   RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_MOD,
+        RESET  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,  RGB_TOG, KC_NO  , KC_NO  , KC_NO  , RGB_M_P, RGB_M_SW,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-          KC_NO, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,   KC_NO  , RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_RMOD,
+        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,  RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, RGB_M_G,
     //├────────┼────────┼────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┼────────┼────────┤
-          KC_NO, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+        KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   , RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_M_R,
     //└────────┴────────┴────────┼────────┼────────┼─────────┤├────────┼────────┼────────┼────────┴────────┴────────┘
-                                   KC_NO  , KC_NO  , KC_NO   ,  RGB_MOD, TG(3) , KC_TRNS
+                                   KC_NO  , KC_TRNS, KC_TRNS ,  RGB_MOD, KC_TRNS, KC_TRNS
     //                           └────────┴────────┴─────────┘└────────┴────────┴────────┘
   )
 };
